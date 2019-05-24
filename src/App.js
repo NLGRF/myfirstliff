1930863c87f3efbab049cacf969cec6c68be1437
NLGRF <non.devtech@gmail.com>
2019-05-25 03:19:03 +0700

    v0.1.0

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'muicss/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+const liff = window.liff;  
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayName : '',
+      userId : '',
+      pictureUrl : '',
+      statusMessage : ''
+    };
+    this.initialize = this.initialize.bind(this);
+    this.closeApp = this.closeApp.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('load', this.initialize);
+  }
+
+  initialize() {
+    liff.init(async (data) => {
+      let profile = await liff.getProfile();
+      this.setState({
+        displayName : profile.displayName,
+        userId : profile.userId,
+        pictureUrl : profile.pictureUrl,
+        statusMessage : profile.statusMessage
+      });
+    }); 
+  }
+
+  closeApp(event) {
+    event.preventDefault();
+    liff.sendMessages([{
+      type: 'text',
+      text: "Thank you, Bye!"
+    }]).then(() => {
+      liff.closeWindow();
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        
+        <p className="App-intro">
+          Display Name : {this.state.displayName} <br/>
+          User ID : {this.state.userId} <br/>
+          Status Msg : {this.state.statusMessage}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <Button color="primary" onClick={this.closeApp}>Close</Button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
